4de57be7976c261bb513b20f5856b0911c1e8471
Gu1lhrm <guialmeidamendes@gmail.com>
2022-09-26 16:51:57 -0300

    Updating Projects

---
+++ projects/project.js
@@ -0,0 +1,5 @@
+const openModalButton = document.quarySelector('#open-modal');
+const closeModalButton = document.quarySelector('#close-modal');
+const modal = document.quarySelector('#modal');
+const fade = document.quarySelector('#fade');
+
