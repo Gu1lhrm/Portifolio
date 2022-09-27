b8b5744ac75d04f2f2a622f0b09c1fd579ac1776
Gu1lhrm <guialmeidamendes@gmail.com>
2022-09-27 10:39:48 -0300

    finilizing modal

--- projects/project.js
+++ projects/project.js
@@ -1,5 +1,12 @@
-const openModalButton = document.quarySelector('#open-modal');
-const closeModalButton = document.quarySelector('#close-modal');
-const modal = document.quarySelector('#modal');
-const fade = document.quarySelector('#fade');
+const openModalButton = document.querySelector('#open-modal');
+const closeModalButton = document.querySelector('#close-modal');
+const modal = document.querySelector('#modal');
+const fade = document.querySelector('#fade');
 
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
