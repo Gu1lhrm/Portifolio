22961b8de4fadd12540bff285944e9fdb795af7d
Gu1lhrm <guialmeidamendes@gmail.com>
2022-09-27 14:03:36 -0300

    updating projects

--- projects/project.js
+++ projects/project.js
@@ -10,3 +10,12 @@ const toggleModal = () => {
 [openModalButton, closeModalButton, fade].forEach((el) => {
     el.addEventListener("click", () => toggleModal());
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
