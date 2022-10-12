f3733a5ed1524dceb75ec6339a27f2fbb8155fc3
Gu1lhrm <guialmeidamendes@gmail.com>
2022-10-11 23:27:31 -0300

    Working modal =)

--- projects/project.js
+++ projects/project.js
@@ -1,15 +1,32 @@
-const openModalButton = document.querySelector('#open-modal');
-const closeModalButton = document.querySelector('#close-modal');
-const modal = document.querySelector('#modal');
-const fade = document.querySelector('#fade');
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
 
-const toggleModal = () => {
-    [modal, fade].forEach((el) => el.classList.toggle("hide"));
+var btn = document.getElementById("myBtnEquitarism");
+var btn2 = document.getElementById("myBtnColumbia");
+
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+btn2.onclick = function () {
+  modal2.style.display = "block";
 }
 
-[openModalButton, closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
-});
+span.onclick = function () {
+  modal.style.display = "none";
+}
+span2.onclick = function () {
+  modal2.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if ((event.target == modal) || (event.target == modal2)) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+  }
+}
 
 function myFunction() {
     var x = document.getElementById("myTopnav");
